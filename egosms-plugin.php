89af5362590609a8d8bfefb3778d30e7576dead2
Harops <41590054+Harops@users.noreply.github.com>
2022-03-21 11:16:46 +0300

    Create egosms-plugin.php

---
+++ egosms-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: EgoSMS
+ * Plugin URI: https://egosms.co/
+ * Description: This is the EgoSMS WordPress plugin!
+ * Version: 1.0.0
+ * Author: Arop Boniface
+ * Author URI: https://github.com/ABHarop
+ */
+
+ // Exit if accessessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
